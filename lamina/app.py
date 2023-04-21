7779ed74aecfef80c4a35821f66fe42079e55b36
coenfuse <65540834+coenfuse@users.noreply.github.com>
2023-04-22 01:09:32 +0530

    update docs

--- lamina/app.py
+++ lamina/app.py
@@ -1,5 +1,5 @@
-# define about this module in 50 words
-# ..
+# This module contains implementation of Lamina class that drives the whole app.
+# Here we also setup the root logger, configuration and CLI for the app.
 
 
 # standard imports
@@ -13,7 +13,7 @@ import threading
 import lamina.metadata as meta
 
 # module imports
-from lamina.core.configurators.basic import Configurator
+from lamina.core.configurators.basic import Configurator as CONFIG
 from lamina.core.streams.stream import Stream
 from lamina.utils import stdlog
 from lamina.utils.error import ERC
@@ -24,59 +24,52 @@ from lamina.utils.error import ERC
 
 
 # ==============================================================================
-# TODO : docs
+# The following is the Lamina class that sets up the whole app. Here we initialize 
+# streams, plugins, and other active component. We also set up the root-logger, 
+# configurator and handle the CLI for this app.
 # ==============================================================================
 class Lamina:
 
-    # TODO : docs
-    # --------------------------------------------------------------------------
     def __init__(self):
         self.__config_file = None
-        self.__config: Configurator = None 
         self.__stop_event = threading.Condition()
         self.__stream = Stream()
 
-    # TODO : docs
-    # --------------------------------------------------------------------------
+
     def start(self) -> ERC:
         status = self.__process_commandline_input()
 
         if status == ERC.SUCCESS:
-            self.__config = Configurator()
-            status = self.__config.parse(self.__config_file)
+            status = CONFIG.parse(self.__config_file)
 
         if status == ERC.SUCCESS:
             status = self.__setup_logging()
 
         if status == ERC.SUCCESS:
-            stdlog.info(f"starting {self.__config.get_app_config()['instance']} v{meta.VERS}")
-            status = self.__stream.configure(self.__config)
+            stdlog.info(f"starting {CONFIG.get_app_config()['instance']} v{meta.VERS}")
+            status = self.__stream.configure(CONFIG)
 
         if status == ERC.SUCCESS:
             status = self.__stream.start()
 
         if status == ERC.SUCCESS:
-            stdlog.info(f"running")
+            stdlog.info("running")
             
             with self.__stop_event:
                 self.__stop_event.wait()
 
-            stdlog.info(f"stopping")
+            stdlog.info("stopping")
             status = self.__stream.stop()
 
-        stdlog.info(f"stopped")
+        stdlog.info("stopped")
         return status
 
 
-    # TODO - docs
-    # --------------------------------------------------------------------------
     def stop(self) -> None:
         with self.__stop_event:
             self.__stop_event.notify()
 
 
-    # docs
-    # --------------------------------------------------------------------------
     def __process_commandline_input(self) -> ERC:
         status = ERC.SUCCESS
         parser = argparse.ArgumentParser()
@@ -104,7 +97,7 @@ class Lamina:
         status = ERC.SUCCESS
 
         # setup basics and fetch config
-        config = self.__config.get_app_config().get("log")
+        config = CONFIG.get_app_config().get("log")
         log_fmt = logging.Formatter(
             datefmt = "%Y-%m-%d %H:%M:%S",
             fmt = "%(asctime)s.%(msecs)03d [%(levelname).1s] : LAMINA : %(message)s")
--- lamina/core/configurators/basic.py
+++ lamina/core/configurators/basic.py
@@ -11,83 +11,86 @@ from lamina.utils.error import ERC
 
 
 # ==============================================================================
-# TODO : Docs.
-# I currently do not find any significant reason to use logging library for
-# printing config messages. Since configuration is going to be called very
+# NOTE : I currently do not find any significant reason to use logging library 
+# for printing config messages. Since configuration is going to be called very
 # frequently but it is always going to return only static data. Which can be
 # verified manually by inspecting the file.
+#
+# This is the root configurator for the Lamina application, this is a class that
+# parses and handles the main Lamina's TOML formatted config file. This 
+# configurator is used as the parent and source for all sub-config components
+# throughout the application. Since this configurator is going to be initialized
+# once and used everywhere, it is designed as a Singleton. 
 # ==============================================================================
 class Configurator:
 
-    # docs
-    # --------------------------------------------------------------------------
-    def __init__(self):
-        self.__CNAME: str = "CONFIG   : [MAIN]"
-        self.__data: dict = None
+    NAME: str = "CONFIG   : [MAIN]"
+    DATA: dict= {}
 
-    # docs
-    # --------------------------------------------------------------------------
-    def parse(self, config_path) -> ERC:
+
+    @staticmethod
+    def parse(config_path) -> ERC:
         try:
-            print(f"{self.__CNAME} reading config at - {config_path}")
-            self.__data = tomllib.load(open(config_path, mode = "rb"))
-            # TODO : self.inspect()
+            print(f"{Configurator.NAME} reading config at - {config_path}")
+            Configurator.DATA = tomllib.load(open(config_path, mode = "rb"))
+            
+            # TODO self.__inspect()
             return ERC.SUCCESS
       
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
 
-    # docs
-    # --------------------------------------------------------------------------
-    def get_app_config(self) -> Dict[str, any] | ERC :
+
+    @staticmethod
+    def get_app_config() -> Dict[str, any] | ERC :
         try:
-            return self.__data["lamina"]
+            return Configurator.DATA["lamina"]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
         
-    # docs
-    # --------------------------------------------------------------------------
-    def get_inputs_config(self) -> Dict[str, dict] | ERC:
+
+    @staticmethod
+    def get_inputs_config() -> Dict[str, dict] | ERC:
         try:
-            return self.__data["inputs"]
+            return Configurator.DATA["inputs"]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
     
-    # docs
-    # --------------------------------------------------------------------------
-    def get_that_input_config(self, type: str, name: str) -> Dict[str, any] | ERC:
+
+    @staticmethod
+    def get_that_input_config(type: str, name: str) -> Dict[str, any] | ERC:
         try:
-            return self.__data["inputs"][type][name]
+            return Configurator.DATA["inputs"][type][name]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
 
-    # docs
-    # --------------------------------------------------------------------------
-    def get_outputs_config(self) -> Dict[str, any] | ERC:
+
+    @staticmethod
+    def get_outputs_config() -> Dict[str, any] | ERC:
         try:
-            return self.__data["outputs"]
+            return Configurator.DATA["outputs"]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
         
-    # docs
-    # --------------------------------------------------------------------------
-    def get_that_output_config(self, type: str, name: str) -> Dict[str, any] | ERC:
+
+    @staticmethod
+    def get_that_output_config(type: str, name: str) -> Dict[str, any] | ERC:
         try:
-            return self.__data["outputs"][type][name]
+            return Configurator.DATA["outputs"][type][name]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
 
-    # docs
-    # --------------------------------------------------------------------------
-    def get_stream_config(self) -> Dict[str, any] | ERC:
+    
+    @staticmethod
+    def get_stream_config() -> Dict[str, any] | ERC:
         try:
-            return self.__data["stream"]
+            return Configurator.DATA["stream"]
         except Exception as e:
-            print(f"{self.__CNAME} exception - {e}")
+            print(f"{Configurator.NAME} exception - {e}")
             return ERC.EXCEPTION
